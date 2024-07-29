1866912afdec4c2662addc60a4cbf6d94324fae2
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-07-29 15:02:48 -0500

    🛸 add the orm configuration

---
+++ backend/config/ormconfig.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV !== 'production',
+};
+
+export default config;
