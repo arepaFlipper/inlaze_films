07ba44845db8e8a14dc14db64e7af930f5cf973d
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-07-30 04:15:24 -0500

    💖 style the Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,22 @@
 import logo from '../../public/inlaze_logo.png'
-const Navbar = () => {
+
+type Props = {
+  labels: string[];
+};
+
+const Navbar = ({labels}: Props) => {
   return (
-    <div>
+    <div className="flex justify-evenly">
       <img src={logo} alt="inlaze_logo" />
+      {
+        labels.map((label: string)=>{
+          return (
+            <div className='flex items-center'>
+              <h2 className=''>{label}</h2>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
