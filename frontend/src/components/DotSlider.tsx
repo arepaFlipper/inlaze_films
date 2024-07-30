b459d107cd114ff1ad3fa3890e11760195a7c870
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-07-30 11:49:47 -0500

    💬 add the DotSlider
    🫲 add the Anchor

--- frontend/src/components/DotSlider.tsx
+++ frontend/src/components/DotSlider.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
 const DotSlider = () => {
-  const dots = [1, 2, 3, 4, 5];
+  const [selectedPage, setSelectedPage] = useState("0");
+
+
+  const selectedStyles = `bg-yellow-500 w-9 rounded before:w-6
+          before:h-6 before:rounded-full 
+          before:left-[-50%] before:top-[-50%]`;
   return (
-    <div className="flex space-x-2 w-full h-20px">
-      {dots.map((_, index) => (
-        <span
-          key={index}
-          className="h-2 w-2 bg-yellow rounded-full opacity-50 hover:opacity-100 transition-opacity duration-300"
-        ></span>
-      ))}
+    <div className="flex gap-6 top-[60%] right-7">
+      {[1, 2, 3, 4, 5, 6, 7].map((dot, index) => {
+        return (
+          <AnchorLink
+            className={`${(selectedPage === `${index}`) ? selectedStyles : "bg-gray-500 rounded-full"} w-3 h-3 `}
+            href="#home"
+            onClick={() => setSelectedPage('1')}
+          />
+        )
+      })}
     </div>
-  );
+  )
 };
 
 export default DotSlider;
